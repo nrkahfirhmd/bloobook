162ffca2a1a8a23184fc07fb9298b225dc1c905d
Nurkahfi Amran Rahmada <nurkahfirahmada@gmail.com>
2026-04-16 13:28:47 +0800

    build: bloobook project

---
+++ BlooBook/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  BlooBook
+//
+//  Created by Nurkahfi Rahmada on 16/04/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
